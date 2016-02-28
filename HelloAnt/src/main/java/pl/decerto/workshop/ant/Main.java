eae92c73b1321d83a9f308cf9c57cd16a56b35a2
macnowak <maciej.nowak@decerto.pl>
2016-02-28 21:08:00 +0100

    ant lib

--- HelloAnt/src/main/java/pl/decerto/workshop/ant/Main.java
+++ HelloAnt/src/main/java/pl/decerto/workshop/ant/Main.java
@@ -1,8 +1,14 @@
 package pl.decerto.workshop.ant;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.BasicConfigurator;
+
 public class Main {
 
+	static Logger logger = Logger.getLogger(Main.class);
+
 	public static void main(String[] args) {
-		System.out.println("Hello World of Ant !");
+		BasicConfigurator.configure();
+		logger.info("Hello World of Ant !");
 	}
 }
