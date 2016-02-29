4aca47b3605a27cdd918838e2bda9c6f4391750d
macnowak <maciej.nowak@decerto.pl>
2016-02-29 10:21:19 +0100

    dodanie pom.xml

--- HolidayPlanner/src/main/java/pl/decerto/workshop/holidayPlanner/generator/RequestNumberGenerator.java
+++ HolidayPlanner/src/main/java/pl/decerto/workshop/holidayPlanner/generator/RequestNumberGenerator.java
@@ -2,11 +2,15 @@ package pl.decerto.workshop.holidayPlanner.generator;
 
 import java.time.LocalDateTime;
 import java.util.StringJoiner;
+import java.util.UUID;
 
 public class RequestNumberGenerator {
 
 	public static String generate() {
 		LocalDateTime now = LocalDateTime.now();
-		return new StringJoiner("/").add(String.valueOf(now.getDayOfMonth())).add(String.valueOf(now.getMonth().getValue())).toString();
+		return new StringJoiner("/").add(String.valueOf(now.getDayOfMonth()))
+			.add(String.valueOf(now.getMonth().getValue()))
+			.add(UUID.randomUUID().toString())
+			.toString();
 	}
 }
